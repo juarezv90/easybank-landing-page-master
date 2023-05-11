d2a4bf9eb8c2c64889d57844b12e2fabd139999b
Victor Juarez <juarezv1990@gmail.com>
2023-05-11 08:39:20 -0400

    final product complete.

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,14 @@
 const menu = document.getElementById('burgermenu');
 const mobileMenu = document.getElementById('mobilemenu');
+
+menu.addEventListener('click', function() {
+
+    if(menu.src.substr(menu.src.indexOf("/images/icon-hamburger.svg")) == "/images/icon-hamburger.svg") {
+        menu.src = "/images/icon-close.svg"
+        let classes = mobileMenu.className
+        mobileMenu.className = classes.replace('hidden', '')
+    } else {
+        menu.src = "/images/icon-hamburger.svg"
+        mobileMenu.className += 'hidden'
+    }
+});
