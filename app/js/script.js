6605ff61af281114ae101c95183d8b8afc8d4ed6
Victor Juarez <juarezv1990@gmail.com>
2023-05-11 09:19:44 -0400

    more location image fixes

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,16 @@
-const menu = document.getElementById('burgermenu');
-const mobileMenu = document.getElementById('mobilemenu');
+const menu = document.getElementById("burgermenu");
+const mobileMenu = document.getElementById("mobilemenu");
 
-menu.addEventListener('click', function() {
-
-    if(menu.src.substr(menu.src.indexOf("/images/icon-hamburger.svg")) == "/images/icon-hamburger.svg") {
-        menu.src = "/images/icon-close.svg"
-        let classes = mobileMenu.className
-        mobileMenu.className = classes.replace('hidden', '')
-    } else {
-        menu.src = "/images/icon-hamburger.svg"
-        mobileMenu.className += 'hidden'
-    }
+menu.addEventListener("click", function () {
+  if (
+    menu.src.substr(menu.src.indexOf("/images/icon-hamburger.svg")) ==
+    "/images/icon-hamburger.svg"
+  ) {
+    menu.src = "./images/icon-close.svg";
+    let classes = mobileMenu.className;
+    mobileMenu.className = classes.replace("hidden", "");
+  } else {
+    menu.src = "./images/icon-hamburger.svg";
+    mobileMenu.className += "hidden";
+  }
 });
